8e91778005a5061eff96ca61de0eb33a7c0ee882
Yla Buri <ylaburi@gmail.com>
2021-12-24 20:56:42 -0300

    ajustes para conexão com a api

--- projetoFinal_client/src/app/commons/abstract-http.service.ts
+++ projetoFinal_client/src/app/commons/abstract-http.service.ts
@@ -1,9 +1,6 @@
 import { Injectable } from '@angular/core';
 
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/do';
-import 'rxjs/add/operator/catch';
-import 'rxjs/add/operator/toPromise';
+
 import { HttpClient, HttpHeaders,HttpErrorResponse,  HttpParams } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { catchError, retry, throwError } from 'rxjs';
--- projetoFinal_client/src/app/services/usuario/usuario.service.ts
+++ projetoFinal_client/src/app/services/usuario/usuario.service.ts
@@ -2,15 +2,16 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { retry, catchError } from 'rxjs/operators';
-import { GenericService } from '../commons/generic.service';
-import { Usuario } from '../models/usuario';
+import { Usuario } from 'src/app/models/usuario';
+import { GenericService } from 'src/app/commons/generic.service';
+
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuarioService extends GenericService {
-  private relativePath = '/usuarios/';
+  private relativePath = 'usuarios/';
 
   constructor(http: HttpClient) {
     super(http);
