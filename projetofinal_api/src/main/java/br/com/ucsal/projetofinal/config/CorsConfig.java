db3d11439ee644a404e3dd1123eafc4e9e288fc3
Yla Buri <ylaburi@gmail.com>
2022-01-18 20:46:41 -0300

    alteração cors

--- projetofinal_api/src/main/java/br/com/ucsal/projetofinal/config/CorsConfig.java
+++ projetofinal_api/src/main/java/br/com/ucsal/projetofinal/config/CorsConfig.java
@@ -9,11 +9,36 @@ import org.springframework.web.servlet.config.annotation.*;
 public class CorsConfig implements WebMvcConfigurer {
 
     @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("/").setCachePeriod(0);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:4200")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+            }
+        };
+    }
+
+    @Override
     public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true)
+                .maxAge(3600);
 
         registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedMethods("*")
-                .allowedHeaders("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS");
+                .allowedHeaders("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true)
+                .maxAge(3600);
     }
 }
