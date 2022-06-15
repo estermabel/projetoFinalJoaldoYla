3a47931534391b74a88b5bd53c84dbbe15216daa
Yla Buri <ylaburi@gmail.com>
2022-06-15 12:02:26 -0300

    ajustes para o cors

--- projetofinal_api/src/main/java/br/com/ucsal/projetofinal/dto/RespostaResponseDto.java
+++ projetofinal_api/src/main/java/br/com/ucsal/projetofinal/dto/RespostaResponseDto.java
@@ -6,21 +6,23 @@ import java.time.LocalDateTime;
 
 public class RespostaResponseDto {
 
+    private Long id;
     private String codigo;
     private LocalDateTime dataEnvio;
     private Long usuarioId;
     private Long tarefaId;
 
     public RespostaResponseDto(Resposta resposta) {
+        this.id = resposta.getId();
         this.codigo = resposta.getCodigo();
         this.dataEnvio = resposta.getDataEnvio();
         this.usuarioId = resposta.getUsuario().getId();
         this.tarefaId = resposta.getTarefa().getId();
     }
 
-    public String getCodigo() {
-        return codigo;
-    }
+    public Long getId() { return id; }
+
+    public String getCodigo() { return codigo; }
 
     public LocalDateTime getDataEnvio() {
         return dataEnvio;
