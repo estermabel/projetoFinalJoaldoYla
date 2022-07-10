a57b733da1994c928c217ac53e56034a84f6dc8d
joaldotavares <joaldo.junior@ucsal.edu.br>
2022-07-10 18:31:58 -0300

    refatoração do resultado

--- projetofinal_api/src/main/java/br/com/ucsal/projetofinal/dto/ResultadoRequestDto.java
+++ projetofinal_api/src/main/java/br/com/ucsal/projetofinal/dto/ResultadoRequestDto.java
@@ -1,50 +1,63 @@
 package br.com.ucsal.projetofinal.dto;
 
-import br.com.ucsal.projetofinal.JavaExecutor;
 import br.com.ucsal.projetofinal.model.CasoTeste;
 import br.com.ucsal.projetofinal.model.Resposta;
 import br.com.ucsal.projetofinal.model.Resultado;
+import br.com.ucsal.projetofinal.model.Teste;
 import br.com.ucsal.projetofinal.repository.CasoTesteRepository;
 import br.com.ucsal.projetofinal.repository.RespostaRepository;
+
+import java.util.List;
 
 public class ResultadoRequestDto {
 
     private String saidaObtida;
-    private Boolean resultado;
+    private Boolean create;
+    private Boolean compile;
+    private Double porcentagem;
     private Long respostaId;
-    private Long casoTesteId;
-    private JavaExecutor javaExecutor = new JavaExecutor();
+    private List<Teste> testes;
+    //private JavaExecutor javaExecutor = new JavaExecutor();
 
     public ResultadoRequestDto() {
     }
 
-    public ResultadoRequestDto(String saidaObtida, Boolean resultado, Long respostaId, Long casoTesteId) {
+    public ResultadoRequestDto(String saidaObtida, Boolean create, Boolean compile, Double porcentagem, Long respostaId, List<Teste> testeId) {
         this.saidaObtida = saidaObtida;
-        this.resultado = resultado;
+        this.create = create;
+        this.compile = compile;
+        this.porcentagem = porcentagem;
         this.respostaId = respostaId;
-        this.casoTesteId = casoTesteId;
+        this.testes = testeId;
     }
 
-    public Resultado toModel(RespostaRepository respostaRepository, CasoTesteRepository casoTesteRepository) {
+    public Resultado toModel(RespostaRepository respostaRepository) {
         Resposta resposta = respostaRepository.findById(respostaId).orElseThrow(() -> new RuntimeException("Id de resposta não encontrado"));
-        CasoTeste casoTeste = casoTesteRepository.findById(casoTesteId).orElseThrow(() -> new RuntimeException("Id de caso de teste não encontrado"));
-        javaExecutor.start(resposta.getCodigo(), casoTeste);
-        return new Resultado(javaExecutor.getSaida(), javaExecutor.getTeste(), resposta, casoTeste);
+        //javaExecutor.start(resposta.getCodigo(), casoTeste);
+        return new Resultado(saidaObtida, create, compile, porcentagem, resposta, testes);
     }
 
     public String getSaidaObtida() {
         return saidaObtida;
     }
 
-    public Boolean getResultado() {
-        return resultado;
+    public Boolean getCreate() {
+        return create;
+    }
+
+    public Boolean getCompile() {
+        return compile;
+    }
+
+    public Double getPorcentagem() {
+        return porcentagem;
     }
 
     public Long getRespostaId() {
         return respostaId;
     }
 
-    public Long getCasoTesteId() {
-        return casoTesteId;
+    public List<Teste> getTestes() {
+        return testes;
     }
 }
