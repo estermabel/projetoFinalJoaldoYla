9d9c163c2e480f488afa4ba563ed64f2d1fd2e04
joaldotavares <joaldo.junior@ucsal.edu.br>
2022-09-11 18:35:09 -0300

    retornando id do usuario no payload ao fazer autenticação

--- projetofinal_api/src/main/java/br/com/ucsal/projetofinal/config/autenticacao/AutenticacaoController.java
+++ projetofinal_api/src/main/java/br/com/ucsal/projetofinal/config/autenticacao/AutenticacaoController.java
@@ -3,6 +3,8 @@ package br.com.ucsal.projetofinal.config.autenticacao;
 import br.com.ucsal.projetofinal.config.token.LoginForm;
 import br.com.ucsal.projetofinal.config.token.TokenDTO;
 import br.com.ucsal.projetofinal.config.token.TokenService;
+import br.com.ucsal.projetofinal.usuario.Usuario;
+import br.com.ucsal.projetofinal.usuario.UsuarioService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -11,6 +13,7 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.Optional;
 
 @CrossOrigin(origins = "*")
 @RestController
@@ -21,20 +24,22 @@ public class AutenticacaoController {
 
     private final TokenService tokenService;
 
-    public AutenticacaoController(AuthenticationManager authenticationManager, TokenService tokenService) {
-        super();
+    private final UsuarioService usuarioService;
+
+    public AutenticacaoController(AuthenticationManager authenticationManager, TokenService tokenService, UsuarioService usuarioService) {
         this.authenticationManager = authenticationManager;
         this.tokenService = tokenService;
+        this.usuarioService = usuarioService;
     }
 
     @PostMapping("/")
     public ResponseEntity<TokenDTO> autenticarLogin(@RequestBody @Valid LoginForm login){
         UsernamePasswordAuthenticationToken dados = login.converter();
+        Usuario usuario = usuarioService.listarPorLogin(dados.getPrincipal().toString());
         try {
             Authentication authentication = authenticationManager.authenticate(dados);
             String token = tokenService.gerarToken(authentication);
-            System.out.println(token);
-            return ResponseEntity.ok(new TokenDTO(token, "Bearer"));
+            return ResponseEntity.ok(new TokenDTO(usuario.getId(), token, "Bearer"));
 
         } catch (AuthenticationException e) {
             return ResponseEntity.badRequest().build();
