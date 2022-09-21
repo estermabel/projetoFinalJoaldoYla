1cbbcccab565f4b5914b692e2b68a84ee82d41ab
joaldotavares <joaldo.junior@ucsal.edu.br>
2022-09-20 22:50:41 -0300

    mudando listagem de tarefa por id para native query para deploy

--- projetofinal_api/src/main/java/br/com/ucsal/projetofinal/tarefa/TarefaRepository.java
+++ projetofinal_api/src/main/java/br/com/ucsal/projetofinal/tarefa/TarefaRepository.java
@@ -1,9 +1,11 @@
 package br.com.ucsal.projetofinal.tarefa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
+    @Query(value = "SELECT * from tarefa inner join caso_teste on tarefa.id = caso_teste.tarefa_id where tarefa.id = :id", nativeQuery = true)
     Optional<Tarefa> findById(Long id);
 }
