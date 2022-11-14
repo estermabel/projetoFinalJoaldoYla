6d2455efb9f83925418cc2ddc6f4c9017ae3ebcd
Yla Buri <ylaburi@gmail.com>
2022-11-14 16:32:49 -0300

    mapeando nova exception

--- projetofinal_api/src/main/java/br/com/ucsal/projetofinal/testcode/ExceptionEnum.java
+++ projetofinal_api/src/main/java/br/com/ucsal/projetofinal/testcode/ExceptionEnum.java
@@ -6,11 +6,12 @@ import java.util.Optional;
 public enum ExceptionEnum {
 
     ARITHMETIC ("java.lang.ArithmeticException", "Você pode ter tentado realizar uma divisão por zero.\n\nTalvez o resultado de sua operação não possa ser representado como um número de ponto flutuante."),
-    ARRAY_INDEX("java.lang.ArrayIndexOutOfBoundsException", "Você tentou acessar um índice menor que zero, ou igual ou maior que o tamanho total da sua matriz."),
+    ARRAY_INDEX("java.lang.ArrayIndexOutOfBoundsException", "Você tentou acessar um índice menor que zero, ou maior que o tamanho total do seu vetor."),
     NULL_POINTER("java.lang.NullPointerException", "Talvez você esteja tentando acessar um índice com valor nulo na sua matriz.\n\nTentando passar parâmetros nulo para o seu método.\n\nTentou acessar um campo de uma instância cuja referência seja null.\n\nVocê pode ter tentando obter uma referência a um método de um objeto nulo."),
     STRING_INDEX_BOUND("java.lang.StringIndexOutOfBoundsException","Talvez o índice que você está tentando acessar não está no intervalo da string, ou seja, negativo ou maior que o último índice da string."),
     ILEGAL_ARGUMENT("java.lang.IllegalArgumentException", "Os argumentos passados para o seu método estão fora do intervalo.\n\nTalvez o formato do valor que está tentando passar seja inválido.\n\nVocê pode está tentando receber um objeto com valor nulo."),
-    NUMBER_FORMAT("java.lang.NumberFormatException", "Variavel do tipo numérico tenta receber tipo de dado inválido");
+    NUMBER_FORMAT("java.lang.NumberFormatException", "Uma variavel do tipo numérico está tentando receber um tipo de dado inválido.\n\nUma letra ou símbolo especial não pode ser convertido para um valor numérico\n\nValores "),
+    INPUT_MISMATCH("java.lang.InputMismatchException", "Talvez o tipo esperado de dados esperado pelo Scanner seja diferente do tipo informado.\n\nTalvez você esteja digitando o separador decimal diferente do esperado pelo seu sistema. Normalmente o separador decimal é representado por uma vírgula e não um ponto.");
     //    ILEGAL_STATE("java.lang.IllegalStateException", ""),
 //    CLASS_CAST("java.lang.ClassCastException", ""),
 
